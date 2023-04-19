2099a08d6aff3f1b6e2537300b14373b711e8057
ashwinihegde28 <ashwinihegde28@gmail.com>
2023-04-18 20:16:04 -0400

    Added daylist to App.js

---
+++ src/components/DayList.js
@@ -0,0 +1,17 @@
+import React from "react";
+import DayListItem from "components/DayListItem.js";
+
+// DayList renders a list of DayListItem components
+export default function DayList(props) {
+  const schedule = props.days.map((dayObj) => {
+    return (
+      <DayListItem
+        key={dayObj.id}
+        selected={dayObj.name === props.day}
+        setDay={props.setDay}
+        {...dayObj}
+      />
+    );
+  });
+  return <ul>{schedule}</ul>;
+}
